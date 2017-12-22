a17c40b2b1cbaa2ad965b69ce1a33e029b696740
NekIP <Shestakov.rm@list.ru>
2017-12-22 19:40:43 +0300

    Setting webpack config
    I created for test 'tes.vue' file. Imported it in 'app.js' and compile project. And it work! But i removed 'test.vue' file

--- PredictionOfCurrencyNeuralNetworks/webpack.config.js
+++ PredictionOfCurrencyNeuralNetworks/webpack.config.js
@@ -1,9 +1,21 @@
 ﻿const path = require('path');
+let UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // плагин минимизации
 
 module.exports = {
-  entry: './wwwroot/app.js',
-  output: {
-    path: path.resolve(__dirname, 'wwwroot/dist'),
-    filename: 'build.js'
-  }
+    entry: './wwwroot/app.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        // new UglifyJSPlugin(), // скрипты будут минимизироваться
+    ]
 };
