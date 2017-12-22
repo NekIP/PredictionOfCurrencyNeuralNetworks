43f50315e5f359d72ef953a92b75fed4e086c771
NekIP <Shestakov.rm@list.ru>
2017-12-22 20:02:25 +0300

    Added pakages for sass and css

--- PredictionOfCurrencyNeuralNetworks/webpack.config.js
+++ PredictionOfCurrencyNeuralNetworks/webpack.config.js
@@ -12,6 +12,18 @@ module.exports = {
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader', 'resolve-url-loader']
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.otf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
             }
         ]
     },
