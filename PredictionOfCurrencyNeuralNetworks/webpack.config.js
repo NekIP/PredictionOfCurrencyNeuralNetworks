d1cc3c82b623aca1068a1e45572df3feb7988f64
NekIP <Shestakov.rm@list.ru>
2017-12-22 17:19:33 +0300

    Setting webpack

--- PredictionOfCurrencyNeuralNetworks/webpack.config.js
+++ PredictionOfCurrencyNeuralNetworks/webpack.config.js
@@ -1,1 +1,9 @@
-﻿
+﻿const path = require('path');
+
+module.exports = {
+  entry: './wwwroot/app.js',
+  output: {
+    path: path.resolve(__dirname, 'wwwroot/dist'),
+    filename: 'build.js'
+  }
+};
