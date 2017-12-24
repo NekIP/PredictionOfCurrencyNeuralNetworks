da763722639787edd04f156de9ef3d19a932a541
NekIP <Shestakov.rm@list.ru>
2017-12-24 16:11:10 +0300

    Added simple neural network class

---
+++ NeuralNetwork/SimpleNeuralNetwork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    public class SimpleNeuralNetwork : NeuralNetwork
+    {
+        public override double[] Run(double[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override NeuralNetworkLearnResult Learn(double[] input, double[] ideal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
