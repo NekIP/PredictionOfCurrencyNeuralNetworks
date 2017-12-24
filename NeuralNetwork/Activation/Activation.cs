0223b610523546b833a4f144172dd25a907cbef2
NekIP <Shestakov.rm@list.ru>
2017-12-24 16:18:50 +0300

    Small refactor

--- NeuralNetwork/Activation/Activation.cs
+++ NeuralNetwork/Activation/Activation.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NeuralNetwork.ActivationFunctions
+namespace NeuralNetwork
 {
-    public abstract class ActivationFunction
+    public abstract class Activation
     {
         /// <summary>
         /// Activation function convert input value in to range [0, 1] (if sigmoid) or [-1, 1] (if hyperbolic).
         /// coefficient - coefficient of coverage of input values
         /// </summary>
-        public Func<double, double, double> Activation { get; set; }
+        public Func<double, double, double> Convert { get; set; }
 
         /// <summary>
         /// Converts input values from [0, 1] (sigmoid) or [-1, 1] (hyperbolic), in to initial values
         /// </summary>
-        public Func<double, double, double> InverseActivation { get; set; }
+        public Func<double, double, double> InverseConvert { get; set; }
     }
 }
