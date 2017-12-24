64bcdcff9ca913519deb51b0ff1b2356d4673c96
NekIP <Shestakov.rm@list.ru>
2017-12-24 16:20:24 +0300

    Added user activation

---
+++ NeuralNetwork/Activation/UserActivation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    public class UserActivation : Activation
+    {
+        public UserActivation(Func<double, double, double> activationFunction,
+            Func<double, double, double> inverseActivationFunction)
+        {
+            ActivationFunction = activationFunction;
+            InverseActivationFunction = inverseActivationFunction;
+        }
+    }
+}
