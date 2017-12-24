2e5acf3b05a99d24cc9368a17daaa366e537b433
NekIP <Shestakov.rm@list.ru>
2017-12-24 16:16:12 +0300

    Added abstract activation class

---
+++ NeuralNetwork/Activation/Activation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork.ActivationFunctions
+{
+    public abstract class ActivationFunction
+    {
+        /// <summary>
+        /// Activation function convert input value in to range [0, 1] (if sigmoid) or [-1, 1] (if hyperbolic).
+        /// coefficient - coefficient of coverage of input values
+        /// </summary>
+        public Func<double, double, double> Activation { get; set; }
+
+        /// <summary>
+        /// Converts input values from [0, 1] (sigmoid) or [-1, 1] (hyperbolic), in to initial values
+        /// </summary>
+        public Func<double, double, double> InverseActivation { get; set; }
+    }
+}
