9b5c34a5fc261e5e6e4ad95414f6b9a07111290e
NekIP <Shestakov.rm@list.ru>
2017-12-24 20:23:48 +0300

    Added weight class

---
+++ NeuralNetwork/Weight.cs
@@ -0,0 +1,20 @@
+﻿namespace NeuralNetwork
+{
+    public class Weight
+    {
+        /// <summary>
+        /// Weight
+        /// </summary>
+        public double W { get; set; }
+
+        /// <summary>
+        /// Delta needed for learning
+        /// </summary>
+        public double D { get; set; }
+
+        public override string ToString()
+        {
+            return W.ToString();
+        }
+    }
+}
