fc7af369a3818563f15d8f9e6af0f7d4c230e293
NekIP <Shestakov.rm@list.ru>
2017-12-24 16:09:40 +0300

    Small documentation

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -4,23 +4,35 @@ namespace NeuralNetwork
 {
     public abstract class NeuralNetwork
     {
-        public Func<double, double, double> Activation { get; private set; }
+        /// <summary>
+        /// Activation function convert input value in to range [0, 1] (if sigmoid) or [-1, 1] (if hyperbolic).
+        /// coefficient - coefficient of coverage of input values
+        /// </summary>
+        public Func<double, double, double> Activation { get; set; }
 
+        /// <summary>
+        /// Converts input values from [0, 1] (sigmoid) or [-1, 1] (hyperbolic), in to initial values
+        /// </summary>
+        public Func<double, double, double> InverseActivation { get; set; }
+
+        /// <summary>
+        /// Run a neural network for input values
+        /// </summary>
         public abstract double[] Run(double[] input);
 
         public abstract NeuralNetworkLearnResult Learn(double[] input, double[] ideal);
 
-        protected double SigmoidActivation(double input, double koef = 1) => 1 / (1 + Math.Exp(-koef * input));
+        protected double SigmoidActivation(double input, double coefficient = 1) => 1 / (1 + Math.Exp(-coefficient * input));
 
-        protected double InversSigmoidActivation(double input, double koef = 1) => -Math.Log(1 / input - 1) / koef;
+        protected double InverseSigmoidActivation(double input, double coefficient = 1) => -Math.Log(1 / input - 1) / coefficient;
 
-        protected double HyperbolicActivation(double input, double koef = 1)
+        protected double HyperbolicActivation(double input, double coefficient = 1)
         {
-            var exp = Math.Exp(2 * koef * input);
+            var exp = Math.Exp(2 * coefficient * input);
             return (exp - 1) / (exp + 1);
         }
 
-        protected double InversHyperbolicActivation(double input, double koef = 1) => 
-            Math.Log((1 + 2 * input) / (1 - 2 * input)) / (2 * koef);
+        protected double InverseHyperbolicActivation(double input, double coefficient = 1) => 
+            Math.Log((1 + 2 * input) / (1 - 2 * input)) / (2 * coefficient);
     }
 }
