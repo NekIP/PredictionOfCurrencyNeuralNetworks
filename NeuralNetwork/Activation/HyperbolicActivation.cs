eba3f2732c840851b6305e4449fcd8fe09a9a311
NekIP <Shestakov.rm@list.ru>
2017-12-24 16:36:49 +0300

    Added implementation for hyperbolic activation

--- NeuralNetwork/Activation/HyperbolicActivation.cs
+++ NeuralNetwork/Activation/HyperbolicActivation.cs
@@ -1,6 +1,22 @@
-﻿namespace NeuralNetwork
+﻿using System;
+
+namespace NeuralNetwork
 {
-    class HyperbolicActivation
+    public class HyperbolicActivation : Activation
     {
+        public HyperbolicActivation()
+        {
+            ActivationFunction = HyperbolicActivationFunction;
+            InverseActivationFunction = InverseHyperbolicActivationFunction;
+        }
+
+        private double HyperbolicActivationFunction(double input, double coefficient = 1)
+        {
+            var exp = Math.Exp(2 * coefficient * input);
+            return (exp - 1) / (exp + 1);
+        }
+
+        private double InverseHyperbolicActivationFunction(double input, double coefficient = 1) =>
+            Math.Log((1 + 2 * input) / (1 - 2 * input)) / (2 * coefficient);
     }
 }
