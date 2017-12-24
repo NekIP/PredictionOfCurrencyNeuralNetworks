28d5440dcf4a6ae559f9c14ed5d2ae0501d48438
NekIP <Shestakov.rm@list.ru>
2017-12-24 18:51:14 +0300

    Added experiment

---
+++ Experiment/Experiments/UnsafeReferenceMatrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment.Experiments
+{
+    public class UnsafeReferenceMatrix : Experiment
+    {
+        public override void Run()
+        {
+            
+        }
+    }
+}
