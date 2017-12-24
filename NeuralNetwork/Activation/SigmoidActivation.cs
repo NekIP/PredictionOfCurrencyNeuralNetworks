5adc8ee594423ec871effd20273e185e95e343b0
NekIP <Shestakov.rm@list.ru>
2017-12-24 16:23:08 +0300

    Added implementation for sigmoid

--- NeuralNetwork/Activation/SigmoidActivation.cs
+++ NeuralNetwork/Activation/SigmoidActivation.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace NeuralNetwork
 {
-    public class SigmoidActivation
+    public class SigmoidActivation : Activation
     {
+        public SigmoidActivation()
+        {
+            ActivationFunction = SigmoidActivationFunction;
+            InverseActivationFunction = InverseSigmoidActivationFunction;
+        }
+
+        private double SigmoidActivationFunction(double input, double coefficient = 1) => 
+            1 / (1 + Math.Exp(-coefficient * input));
+
+        private double InverseSigmoidActivationFunction(double input, double coefficient = 1) => 
+            -Math.Log(1 / input - 1) / coefficient;
     }
 }
