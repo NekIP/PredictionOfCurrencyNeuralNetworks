a2eafa26b7f83abfada3f27f63db61bab4fe1d19
NekIP <Shestakov.rm@list.ru>
2017-12-25 19:24:15 +0300

    Implementation tanspose matrix

--- NeuralNetwork/Math/MathHelper.cs
+++ NeuralNetwork/Math/MathHelper.cs
@@ -6,5 +6,18 @@ namespace NeuralNetwork.Math
 {
     public class MathHelper
     {
+        public T[][] Transpose<T>(T[][] matrix)
+        {
+            var result = new T[matrix.GetLength(1)][];
+            for (var i = 0; i < matrix.GetLength(1); i++)
+            {
+                result[i] = new T[matrix.GetLength(0)];
+                for (var j = 0; j < matrix.GetLength(0); j++)
+                {
+                    result[i][j] = matrix[j][i];
+                }
+            }
+            return result;
+        }
     }
 }
