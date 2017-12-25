8ac6809f596dab82f84577d4b16275ca6bfff12e
NekIP <Shestakov.rm@list.ru>
2017-12-25 21:12:34 +0300

    Added test

--- Experiments/Experiments/NeuralNetworkMatrixTest.cs
+++ Experiments/Experiments/NeuralNetworkMatrixTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NeuralNetwork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,55 @@ namespace Experiment
     {
         public override void Run()
         {
-            
+            var nt = new PerceptronNeuralNetwork(
+                new PeceptronNeuralNetworkParameters(),
+                new SigmoidActivation(),
+                2, 3, 2);
+            PrintMatrix(nt.Neurons);
+            Console.WriteLine("Weights: ");
+            PrintMatrix(nt.Weights);
+            Console.WriteLine("Weights Transpose: ");
+            PrintMatrix(nt.WeightsTranspose);
+
+            nt.Weights[0][0][0].W = -1;
+            Console.WriteLine("Weights: ");
+            PrintMatrix(nt.Weights);
+            Console.WriteLine("Weights Transpose: ");
+            PrintMatrix(nt.WeightsTranspose);
+
+            nt.WeightsTranspose[0][0][1].W = -2;
+            Console.WriteLine("Weights: ");
+            PrintMatrix(nt.Weights);
+            Console.WriteLine("Weights Transpose: ");
+            PrintMatrix(nt.WeightsTranspose);
+        }
+
+        private void PrintMatrix(double[][] matrix)
+        {
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                for (var j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintMatrix(Weight[][][] matrix)
+        {
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine("Layer " + i);
+                for (var j = 0; j < matrix[i].Length; j++)
+                {
+                    for (var k = 0; k < matrix[i][j].Length; k++)
+                    {
+                        Console.Write(matrix[i][j][k] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
