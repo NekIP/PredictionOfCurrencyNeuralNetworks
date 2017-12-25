928def2b410fb857265559ba827b4aa4900bc899
NekIP <Shestakov.rm@list.ru>
2017-12-25 18:10:57 +0300

    Added math struct

---
+++ NeuralNetwork/Math/Matrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+namespace NeuralNetwork.Math
+{
+    public class Matrix : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NeuralNetwork/Math/Vector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork.Math
+{
+    public class Vector
+    {
+
+    }
+}
