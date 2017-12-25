bf1c3c153eb7ece2bb6074a695985f47adee87d5
NekIP <Shestakov.rm@list.ru>
2017-12-25 19:49:55 +0300

    Change project from .net framework to .net core

---
+++ Experiments/Experiments/NeuralNetworkMatrixTest.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment
+{
+    public class NeuralNetworkMatrixTest : Experiment
+    {
+        public override void Run()
+        {
+            
+        }
+    }
+}
---
+++ Experiments/Experiments/UnsafeReferenceMatrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment
+{
+    public unsafe class UnsafeReferenceMatrix : Experiment
+    {
+        public class Ref<T>
+        {
+            public T V;
+
+            public Ref(T value)
+            {
+                V = value;
+            }
+
+            public override string ToString()
+            {
+                return V.ToString();
+            }
+        }
+
+        public unsafe override void Run()
+        {
+
+        }
+    }
+}
