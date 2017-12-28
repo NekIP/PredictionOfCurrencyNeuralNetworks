50a2ba8da58ce3d288ea1dd6497192a34a52be80
NekIP <Shestakov.rm@list.ru>
2017-12-28 20:28:10 +0300

    Added a new implementation for matrix and vector

---
+++ NeuralNetwork/Structs/RectangleMatrix.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+
+namespace NeuralNetwork {
+	public class RectangleMatrix {
+		public Vector[] Vectors { get; private set; }
+
+		public int RowCount => Vectors.Length;
+		public int ColumnCount => Vectors.First().Length;
+
+		public RectangleMatrix(double[][] values) {
+			var сountElementsInFirstVectors = values.First().Length;
+			Vectors = new Vector[values.GetLength(0)];
+			for (var i = 0; i < Vectors.Length; i++) {
+				Vectors[i] = new Vector(values[i]);
+				if (values[i].Length != сountElementsInFirstVectors) {
+					throw new Exception("Matrix must be rectangular");
+				}
+			}
+		}
+
+		public RectangleMatrix(int countRow, int countColumn) {
+			Vectors = new Vector[countRow];
+			for (var i = 0; i < Vectors.Length; i++) {
+				Vectors[i] = new Vector(countColumn);
+			}
+		}
+
+		public Vector this[int i] {
+			get {
+				return Vectors[i];
+			}
+			set {
+				Vectors[i] = value;
+			}
+		}
+
+		public double this[int i, int j] {
+			get {
+				return Vectors[i][j];
+			}
+			set {
+				Vectors[i][j] = value;
+			}
+		}
+
+		public static RectangleMatrix operator *(RectangleMatrix matrix1, RectangleMatrix matrix2) {
+			if (matrix1.ColumnCount != matrix2.RowCount) {
+				throw new ArithmeticException("Count column of matrix1 and count row of matrix2 must be equals");
+			}
+			var result = new RectangleMatrix(matrix1.RowCount, matrix1.ColumnCount);
+			for (int i = 0; i < matrix1.RowCount; i++) {
+				for (int j = 0; j < matrix2.ColumnCount; j++) {
+					for (int k = 0; k < matrix2.RowCount; k++) {
+						result[i, j] += matrix1[i, k] * matrix2[k, j];
+					}
+				}
+			}
+			return result;
+		}
+
+		public static Vector operator *(RectangleMatrix matrix, Vector vector) {
+			if (matrix.ColumnCount != vector.Length) {
+				throw new ArithmeticException("Count column of matrix1 and count row of matrix2 must be equals");
+			}
+			var result = new Vector(vector.Length);
+			for (int i = 0; i < matrix.RowCount; i++) {
+				for (int j = 0; j < vector.Length; j++) {
+					result[i] += matrix[i, j] * vector[j];
+				}
+			}
+			return result;
+		}
+
+		public static RectangleMatrix operator -(RectangleMatrix matrix) => Convert(matrix, x => -x);
+
+
+		public static RectangleMatrix operator +(RectangleMatrix matrix1, RectangleMatrix matrix2) =>
+			Combine(matrix1, matrix2, (x1, x2) => x1 + x2);
+
+		public static RectangleMatrix operator -(RectangleMatrix matrix1, RectangleMatrix matrix2) =>
+			Combine(matrix1, matrix2, (x1, x2) => x1 - x2);
+
+		public static RectangleMatrix Combine(RectangleMatrix matrix1, RectangleMatrix matrix2,
+			Func<double, double, double> converter) {
+			if (matrix1.RowCount != matrix2.RowCount || matrix1.ColumnCount != matrix2.ColumnCount) {
+				throw new ArithmeticException("Count row and column of matrix must be equals");
+			}
+			var result = new RectangleMatrix(matrix1.RowCount, matrix1.ColumnCount);
+			for (var i = 0; i < matrix1.RowCount; i++) {
+				result[i] = Vector.Combine(matrix1[i], matrix2[i], converter);
+			}
+			return result;
+		}
+
+		public static RectangleMatrix Convert(RectangleMatrix matrix,
+			Func<double, double> converter) {
+			var result = new RectangleMatrix(matrix.RowCount, matrix.ColumnCount);
+			for (var i = 0; i < matrix.RowCount; i++) {
+				result[i] = Vector.Convert(matrix[i], converter);
+			}
+			return result;
+		}
+	}
+}
---
+++ NeuralNetwork/Structs/Vector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork {
+	public class Vector {
+		public int Length => Values.Length;
+
+		private double[] Values { get; set; }
+
+		public Vector(double[] values) {
+			Values = values;
+		}
+
+		public Vector(int length) {
+			Values = new double[length];
+		}
+
+		public double this[int index] {
+			get {
+				return Values[index];
+			}
+			set {
+				Values[index] = value;
+			}
+		}
+
+		public static Vector operator +(Vector vector1, Vector vector2) =>
+			Combine(vector1, vector2, (x1, x2) => x1 + x2);
+
+		public static Vector operator -(Vector vector1, Vector vector2) =>
+			Combine(vector1, vector2, (x1, x2) => x1 - x2);
+
+		public static Vector operator *(Vector vector, double scalar) =>
+			Convert(vector, x => x * scalar);
+
+		public static Vector operator -(Vector item) => 
+			Convert(item, x => -x);
+
+		public static Vector Combine(Vector vector1, Vector vector2, 
+			Func<double, double, double> converter) {
+			if (vector1.Length != vector2.Length) {
+				throw new ArithmeticException("Length of vectors must be equals");
+			}
+			var result = new Vector(vector1.Length);
+			for (var i = 0; i < vector1.Length; i++) {
+				result[i] = converter(vector1[i], vector2[i]);
+			}
+			return result;
+		}
+
+		public static Vector Convert(Vector vector,
+			Func<double, double> converter) {
+			var result = new Vector(vector.Length);
+			for (var i = 0; i < vector.Length; i++) {
+				result[i] = converter(vector[i]);
+			}
+			return result;
+		}
+	}
+}
