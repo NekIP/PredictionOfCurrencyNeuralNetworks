ddef383381f06189dae8c79acbb1d4500269446f
NekIP <Shestakov.rm@list.ru>
2018-01-02 20:39:17 +0300

    Added constructor in lstmLayer class

--- NeuralNetwork/Implementation/Recurent/LstmLayer.cs
+++ NeuralNetwork/Implementation/Recurent/LstmLayer.cs
@@ -1,9 +1,30 @@
-﻿namespace NeuralNetwork {
+﻿using System;
+
+namespace NeuralNetwork {
 	public class LstmLayer {
+		public Activation Activation { get; set; }
 		public Matrix Weights { get; set; }
 		public Matrix DeffWeights { get; set; }
 		public Vector Bias { get; set; }
 		public Vector Input { get; set; }
 		public Vector Output { get; set; }
+
+		public LstmLayer(int lengthOfInput, int lengthOfOutput, Activation activation) {
+			Activation = activation;
+			InitVectors(lengthOfInput, lengthOfOutput);
+			InitMatrixes(lengthOfInput, lengthOfOutput);
+		}
+
+		private void InitVectors(int lengthOfInput, int lengthOfOutput) {
+			Input = new Vector(lengthOfInput);
+			Output = new Vector(lengthOfOutput);
+			Bias = new Vector(lengthOfOutput);
+		}
+
+		private void InitMatrixes(int lengthOfInput, int lengthOfOutput) {
+			var rnd = new Random();
+			Weights = new Matrix(lengthOfOutput, lengthOfInput, () => rnd.NextDouble());
+			DeffWeights = new Matrix(lengthOfOutput, lengthOfInput);
+		}
 	}
 }
