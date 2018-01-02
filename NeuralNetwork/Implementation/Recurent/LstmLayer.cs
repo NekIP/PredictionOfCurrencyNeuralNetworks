11922893840ff8405b93c5e9a653e0e595ee056b
NekIP <Shestakov.rm@list.ru>
2018-01-02 20:21:41 +0300

    Added implementation for lstm layer

--- NeuralNetwork/Implementation/Recurent/LstmLayer.cs
+++ NeuralNetwork/Implementation/Recurent/LstmLayer.cs
@@ -1,4 +1,9 @@
 ﻿namespace NeuralNetwork {
 	public class LstmLayer {
+		public Matrix Weights { get; set; }
+		public Matrix DeffWeights { get; set; }
+		public Vector Bias { get; set; }
+		public Vector Input { get; set; }
+		public Vector Output { get; set; }
 	}
 }
