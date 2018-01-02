fd8403d8b2b39a5d32c3300ada42c5c11145b106
NekIP <Shestakov.rm@list.ru>
2018-01-02 20:03:59 +0300

    Small Refactor

--- NeuralNetwork/Common/NeuralNetworkException.cs
+++ NeuralNetwork/Common/NeuralNetworkException.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 
 namespace NeuralNetwork {
-	public class NeuralNetworkException : Exception
-    {
+	public class NeuralNetworkException : Exception {
 		public NeuralNetworkException() { }
 		public NeuralNetworkException(string message) : base(message) { }
 		public NeuralNetworkException(string message, Exception innerException) : base(message, innerException) { }
