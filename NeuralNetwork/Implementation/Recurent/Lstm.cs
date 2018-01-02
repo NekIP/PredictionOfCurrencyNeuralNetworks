9ccbcf355c20e1a6b057f56ebb339c6721e656ff
NekIP <Shestakov.rm@list.ru>
2018-01-02 20:26:04 +0300

    Added properties

--- NeuralNetwork/Implementation/Recurent/Lstm.cs
+++ NeuralNetwork/Implementation/Recurent/Lstm.cs
@@ -1,5 +1,10 @@
 ﻿namespace NeuralNetwork {
 	public class Lstm {
+		public LstmLayer ForgetGateLayer { get; set; }
+		public LstmLayer InputLayerGate { get; set; }
+		public LstmLayer TanhLayer { get; set; }
+		public LstmLayer OutputLayer { get; set; }
+
 		public Lstm(int lengthOfInputLayer, int LengthOutputLayer) {
 
 		}
