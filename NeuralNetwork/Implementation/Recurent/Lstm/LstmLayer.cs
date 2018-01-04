cbaf6f5903bb46746b3d91637a88ee96cc72f586
NekIP <Shestakov.rm@list.ru>
2018-01-04 19:46:44 +0300

    Added initialization

--- NeuralNetwork/Implementation/Recurent/Lstm/LstmLayer.cs
+++ NeuralNetwork/Implementation/Recurent/Lstm/LstmLayer.cs
@@ -4,5 +4,34 @@
 		public SinglelayerPerceptron InputLayerGate { get; set; }
 		public SinglelayerPerceptron TanhLayer { get; set; }
 		public SinglelayerPerceptron OutputLayer { get; set; }
+
+		public LstmLayer(PerceptronParameters perceptronParameters, 
+			int lengthOfInput, int lengthOfOutput) {
+			InitializeNeuralNetworkLayers(perceptronParameters, lengthOfInput, lengthOfOutput);
+		}
+
+		private void InitializeNeuralNetworkLayers(PerceptronParameters perceptronParameters,
+			int lengthOfInput, int lengthOfOutput) {
+			ForgetGateLayer = new SinglelayerPerceptron(
+				perceptronParameters,
+				new SigmoidActivation(),
+				lengthOfInput + lengthOfOutput,
+				lengthOfOutput);
+			InputLayerGate = new SinglelayerPerceptron(
+				perceptronParameters,
+				new SigmoidActivation(),
+				lengthOfInput + lengthOfOutput,
+				lengthOfOutput);
+			TanhLayer = new SinglelayerPerceptron(
+				perceptronParameters,
+				new HyperbolicActivation(),
+				lengthOfInput + lengthOfOutput,
+				lengthOfOutput);
+			OutputLayer = new SinglelayerPerceptron(
+				perceptronParameters,
+				new SigmoidActivation(),
+				lengthOfInput + lengthOfOutput,
+				lengthOfOutput);
+		}
 	}
 }
