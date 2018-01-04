6ea33b2013e7e2192c74a4e88fb49a6c4c92b81d
NekIP <Shestakov.rm@list.ru>
2018-01-04 19:31:22 +0300

    Refactor

--- NeuralNetwork/Implementation/Recurent/Lstm/Lstm.cs
+++ NeuralNetwork/Implementation/Recurent/Lstm/Lstm.cs
@@ -1,7 +1,5 @@
 ﻿namespace NeuralNetwork {
 	public class Lstm : Recurent {
-		
-
 		public Lstm(int lengthOfInputLayer, int LengthOutputLayer) {
 
 		}
@@ -13,13 +11,5 @@
 		public override Vector[] Run(Vector[] input) {
 			throw new System.NotImplementedException();
 		}
-
-		/*public override Vector Run(Vector input) {
-			throw new System.NotImplementedException();
-		}
-
-		public override NeuralNetworkLearnResult Learn(Vector input, Vector ideal) {
-			throw new System.NotImplementedException();
-		}*/
 	}
 }
