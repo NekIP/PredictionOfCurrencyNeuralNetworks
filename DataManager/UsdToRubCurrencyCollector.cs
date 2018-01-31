f2d184597d77ad6d4b06527370eac1a01fca043e
NekIP <Shestakov.rm@list.ru>
2018-01-31 18:07:49 +0300

    Small fix

---
+++ DataManager/UsdToRubCurrencyCollector.cs
@@ -0,0 +1,28 @@
+﻿using DataBase.Entities;
+using DataBase.Repositories;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager {
+	public class UsdToRubCurrencyCollector : DataCollector<Product> {
+		public UsdToRubCurrencyCollector(IConfiguration configuration) {
+			Source = "";
+			Repository = new UsdToRubCurrencyRepository(configuration);
+		}
+
+		public override Task<Product[]> Get() {
+			throw new NotImplementedException();
+		}
+
+		public override Task<Product> Get(DateTime date) {
+			throw new NotImplementedException();
+		}
+
+		public override Task<Product[]> Get(DateTime from, DateTime to) {
+			throw new NotImplementedException();
+		}
+	}
+}
