8761b6352870a125c3bc47b8195070a719c8473a
NekIP <Shestakov.rm@list.ru>
2018-01-31 13:21:37 +0300

    Added database(mongo) project

---
+++ DataBase/Entity.cs
@@ -0,0 +1,8 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DataBase {
+	public class Entity {
+		[BsonId]
+		public long Id { get; set; }
+	}
+}
---
+++ DataBase/Repositories/ProductsRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataBase.Entities;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataBase.Repositories {
+	public interface IProductsRepository : IRepository<Product> {
+		Task<List<Product>> GetMostPopular(int count);
+	}
+
+	public class ProductsRepository : Repository<Product>, IProductsRepository {
+		public ProductsRepository(IConfiguration configuration) : base("products", configuration) { }
+
+		public Task<List<Product>> GetMostPopular(int count) {
+			return this.SortBy(x => x.Rating, SortDirection.Descending).Take(count).Execute();
+		}
+	}
+}
