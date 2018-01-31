f82cf1fd0fd1a4cee09b06fbf7a8f31296ff3de6
NekIP <Shestakov.rm@list.ru>
2018-01-31 16:33:02 +0300

    Implemented loader

---
+++ DataManager/Common/Loader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DataManager {
+	public class Loader {
+		public string Url { get; set; }
+
+		public Loader(string url) {
+			Url = url;
+		}
+
+		public Task<string> Get(params KeyValuePair<string, string>[] parameters) =>
+			RequestTemplate(parameters, "GET");
+
+		public Task<string> Post(params KeyValuePair<string, string>[] parameters) =>
+			RequestTemplate(parameters, "POST");
+
+		public async Task<T> Get<T>(Func<string, T> converter, params KeyValuePair<string, string>[] parameters) =>
+			converter(await Get(parameters));
+
+		public async Task<T> Post<T>(Func<string, T> converter, params KeyValuePair<string, string>[] parameters) =>
+			converter(await Post(parameters));
+
+		private Task<string> RequestTemplate(KeyValuePair<string, string>[] parameters, string method) {
+			var client = new WebClient();
+			var urlParams = GetUrlParameters(parameters);
+			return client.UploadStringTaskAsync(Url, method, urlParams);
+		}
+
+		private string GetUrlParameters(KeyValuePair<string, string>[] parameters) =>
+			parameters.Length > 0
+				? $"?{ String.Join('&', parameters.Select(x => $"{ x.Key }={ x.Value }")) }"
+				: string.Empty;
+	}
+}
