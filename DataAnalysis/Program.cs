148c9e2a70702d3089f97a26a37c9ee8b0097ed1
NekIP <Shestakov.rm@list.ru>
2018-01-31 13:28:56 +0300

    Installed a microsoft.extension.configuration

--- DataAnalysis/Program.cs
+++ DataAnalysis/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
 namespace DataAnalysis {
-	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+	public class Program {
+		public static void Main(string[] args) {
+			
 		}
 	}
 }
