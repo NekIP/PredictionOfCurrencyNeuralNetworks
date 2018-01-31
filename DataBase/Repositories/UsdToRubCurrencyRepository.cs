399e8012da2d6688c484f6665f659990776a9702
NekIP <Shestakov.rm@list.ru>
2018-01-31 18:02:49 +0300

    Smal refactor

---
+++ DataBase/Repositories/UsdToRubCurrencyRepository.cs
@@ -0,0 +1,9 @@
+﻿using DataBase.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace DataBase.Repositories {
+	public interface IUsdToRubCurrencyRepository : IRepository<Product> { }
+	public class UsdToRubCurrencyRepository : Repository<Product>, IUsdToRubCurrencyRepository {
+		public UsdToRubCurrencyRepository(IConfiguration configuration) : base("usdToRub", configuration) { }
+	}
+}
