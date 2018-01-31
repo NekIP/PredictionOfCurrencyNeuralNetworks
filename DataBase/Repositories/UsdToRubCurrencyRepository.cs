2a6f92858299343e770d146c5a97ba7fffe286cc
NekIP <Shestakov.rm@list.ru>
2018-01-31 21:13:24 +0300

    Refactor

--- DataBase/Repositories/UsdToRubCurrencyRepository.cs
+++ DataBase/Repositories/UsdToRubCurrencyRepository.cs
@@ -2,8 +2,8 @@
 using Microsoft.Extensions.Configuration;
 
 namespace DataBase.Repositories {
-	public interface IUsdToRubCurrencyRepository : IRepository<Product> { }
-	public class UsdToRubCurrencyRepository : Repository<Product>, IUsdToRubCurrencyRepository {
-		public UsdToRubCurrencyRepository(IConfiguration configuration) : base("usdToRub", configuration) { }
-	}
+    public interface IUsdToRubCurrencyRepository : IRepository<Product> { }
+    public class UsdToRubCurrencyRepository : Repository<Product>, IUsdToRubCurrencyRepository {
+        public UsdToRubCurrencyRepository(IConfiguration configuration) : base("usdToRubCurrency", configuration) { }
+    }
 }
