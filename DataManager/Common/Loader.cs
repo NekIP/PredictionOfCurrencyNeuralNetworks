33eceb0bad4d126dbef4f162b7fe68f4c9745c57
NekIP <Shestakov.rm@list.ru>
2018-02-01 19:32:09 +0300

    Changed loader

--- DataManager/Common/Loader.cs
+++ DataManager/Common/Loader.cs
@@ -2,33 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DataManager {
 	public class Loader {
-		public string Url { get; set; }
+		public Loader() {}
 
-		public Loader(string url) {
-			Url = url;
-		}
+        public Task<string> Get(string url, params KeyValuePair<string, string>[] parameters) {
+            var client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            var urlParams = GetUrlParameters(parameters);
+            return client.DownloadStringTaskAsync(url + urlParams);
+        }
 
-		public Task<string> Get(params KeyValuePair<string, string>[] parameters) =>
-			RequestTemplate(parameters, "GET");
+		public Task<string> Post(string url, params KeyValuePair<string, string>[] parameters) {
+            var client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            var urlParams = GetUrlParameters(parameters);
+            return client.UploadStringTaskAsync(url, urlParams);
+        }
 
-		public Task<string> Post(params KeyValuePair<string, string>[] parameters) =>
-			RequestTemplate(parameters, "POST");
+        public async Task<T> Get<T>(string url, Func<string, T> converter, params KeyValuePair<string, string>[] parameters) =>
+			converter(await Get(url, parameters));
 
-		public async Task<T> Get<T>(Func<string, T> converter, params KeyValuePair<string, string>[] parameters) =>
-			converter(await Get(parameters));
-
-		public async Task<T> Post<T>(Func<string, T> converter, params KeyValuePair<string, string>[] parameters) =>
-			converter(await Post(parameters));
-
-		private Task<string> RequestTemplate(KeyValuePair<string, string>[] parameters, string method) {
-			var client = new WebClient();
-			var urlParams = GetUrlParameters(parameters);
-			return client.UploadStringTaskAsync(Url, method, urlParams);
-		}
+		public async Task<T> Post<T>(string url, Func<string, T> converter, params KeyValuePair<string, string>[] parameters) =>
+			converter(await Post(url, parameters));
 
 		private string GetUrlParameters(KeyValuePair<string, string>[] parameters) =>
 			parameters.Length > 0
