efdb55269f695cf018a95eeae5028ba29a191f2e
NekIP <Shestakov.rm@list.ru>
2018-02-26 10:44:59 +0300

    Added aproximation

--- DataManager/Common/Interpolation/Approximation.cs
+++ DataManager/Common/Interpolation/Approximation.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace DataManager {
     public abstract class Approximation {
+
         public abstract double Approximate(double x, IDictionary<double, double> nodes);
     }
 }
