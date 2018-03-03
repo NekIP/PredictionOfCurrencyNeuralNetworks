9cf587e6fabfee7d5645939f50931fa489d11e6e
NekIP <Shestakov.rm@list.ru>
2018-03-03 15:51:36 +0300

    Created interface

--- DataManager/DataPreparerForNeuralNetwork.cs
+++ DataManager/DataPreparerForNeuralNetwork.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DataManager
-{
-    class DataPreparerForNeuralNetwork
-    {
+﻿namespace DataManager {
+    public interface IDataPreparerForNeuralNetwork { }
+    public class DataPreparerForNeuralNetwork : IDataPreparerForNeuralNetwork {
+        
     }
 }
