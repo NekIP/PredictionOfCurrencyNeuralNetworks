09123f1839b15832076538f6aeaec23b467e6067
NekIP <Shestakov.rm@list.ru>
2018-03-13 14:48:57 +0300

    Init fields

--- PredictionOfCurrencyNeuralNetworks/Controllers/DataManagerController.cs
+++ PredictionOfCurrencyNeuralNetworks/Controllers/DataManagerController.cs
@@ -11,8 +11,51 @@ using DataManager;
 namespace PredictionOfCurrencyNeuralNetworks.Controllers {
     public class DataManagerController : Controller {
         public IUsdToRubCurrencyCollector UsdToRubCollector { get; set; }
-        public DataManagerController(IUsdToRubCurrencyCollector usdToRubCollector) {
+        public ICAC40Collector Cac40Collector { get; set; }
+        public ICSI200Collector Csi200Collector { get; set; }
+        public IDataForNeuralNetworkCollector DataForNeuralNetworkCollector { get; set; }
+        public IDowJonesCollector DowJonesCollector { get; set; }
+        public IGdpPerCapitaPppCollector GdpPerCapitaPppCollector { get; set; }
+        public IGoldCollector GoldCollector { get; set; }
+        public IInflationCollector InflationCollector { get; set; }
+        public IMMVBCollector MmvbCollector { get; set; }
+        public IOliBrentCollector OliBrentCollector { get; set; }
+        public IOliLightCollector OliLightCollector { get; set; }
+        public IRefinancingRateCollector RefinancingRateCollector { get; set; }
+        public IRTSCollector RtsCollector { get; set; }
+        public ISAndP500Collector SAndPCollector { get; set; }
+        public ITradeBalanceCollector TradeBalanceCollector { get; set; }
+
+        public DataManagerController(IUsdToRubCurrencyCollector usdToRubCollector,
+            ICAC40Collector cac40Collector,
+            ICSI200Collector csi200Collector,
+            IDataForNeuralNetworkCollector dataForNeuralNetworkCollector,
+            IDowJonesCollector dowJonesCollector,
+            IGdpPerCapitaPppCollector gdpPerCapitaPppCollector,
+            IGoldCollector goldCollector,
+            IInflationCollector inflationCollector,
+            IMMVBCollector mmvbCollector,
+            IOliBrentCollector oliBrentCollector,
+            IOliLightCollector oliLightCollector,
+            IRefinancingRateCollector refinancingRateCollector,
+            IRTSCollector rtsCollector,
+            ISAndP500Collector sAndPCollector,
+            ITradeBalanceCollector tradeBalanceCollector) {
             UsdToRubCollector = usdToRubCollector;
+            Cac40Collector = cac40Collector;
+            Csi200Collector = csi200Collector;
+            DataForNeuralNetworkCollector = dataForNeuralNetworkCollector;
+            DowJonesCollector = dowJonesCollector;
+            GdpPerCapitaPppCollector = gdpPerCapitaPppCollector;
+            GoldCollector = goldCollector;
+            InflationCollector = inflationCollector;
+            MmvbCollector = mmvbCollector;
+            OliBrentCollector = oliBrentCollector;
+            OliLightCollector = oliLightCollector;
+            RefinancingRateCollector = refinancingRateCollector;
+            RtsCollector = rtsCollector;
+            SAndPCollector = sAndPCollector;
+            TradeBalanceCollector = tradeBalanceCollector;
         }
 
         // GET: /<controller>/
