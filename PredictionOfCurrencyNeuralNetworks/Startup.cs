c25afbb487ba61e4e092ac21e5277980cafe0030
NekIP <Shestakov.rm@list.ru>
2018-03-13 14:41:26 +0300

    Added initialization dependency injection for collectors and repositories

--- PredictionOfCurrencyNeuralNetworks/Startup.cs
+++ PredictionOfCurrencyNeuralNetworks/Startup.cs
@@ -41,10 +41,38 @@ namespace PredictionOfCurrencyNeuralNetworks {
 
         protected void InitCollectors(IServiceCollection services) {
             services.AddTransient<IUsdToRubCurrencyCollector, UsdToRubCurrencyCollector>();
+            services.AddTransient<ICAC40Collector, CAC40Collector>();
+            services.AddTransient<ICSI200Collector, CSI200Collector>();
+            services.AddTransient<IDataForNeuralNetworkCollector, DataForNeuralNetworkCollector>();
+            services.AddTransient<IDowJonesCollector, DowJonesCollector>();
+            services.AddTransient<IGdpPerCapitaPppCollector, GdpPerCapitaPppCollector>();
+            services.AddTransient<IGoldCollector, GoldCollector>();
+            services.AddTransient<IInflationCollector, InflationCollector>();
+            services.AddTransient<IMMVBCollector, MMVBCollector>();
+            services.AddTransient<IOliBrentCollector, OliBrentCollector>();
+            services.AddTransient<IOliLightCollector, OliLightCollector>();
+            services.AddTransient<IRefinancingRateCollector, RefinancingRateCollector>();
+            services.AddTransient<IRTSCollector, RTSCollector>();
+            services.AddTransient<ISAndP500Collector, SAndP500Collector>();
+            services.AddTransient<ITradeBalanceCollector, TradeBalanceCollector>();
         }
 
         protected void InitRepositories(IServiceCollection services) {
             services.AddTransient<IUsdToRubCurrencyRepository, UsdToRubCurrencyRepository>();
+            services.AddTransient<ICAC40Repository, CAC40Repository>();
+            services.AddTransient<ICSI200Repository, CSI200Repository>();
+            services.AddTransient<IDataForNeuralNetworkRepository, DataForNeuralNetworkRepository>();
+            services.AddTransient<IDowJonesRepository, DowJonesRepository>();
+            services.AddTransient<IGdpPerCapitaPppRepository, GdpPerCapitaPppRepository>();
+            services.AddTransient<IGoldRepository, GoldRepository>();
+            services.AddTransient<IInflationRepository, InflationRepository>();
+            services.AddTransient<IMMVBRepository, MMVBRepository>();
+            services.AddTransient<IOliBrentRepository, OliBrentRepository>();
+            services.AddTransient<IOliLightRepository, OliLightRepository>();
+            services.AddTransient<IRefinancingRateRepository, RefinancingRateRepository>();
+            services.AddTransient<IRTSRepository, RTSRepository>();
+            services.AddTransient<ISAndPRepository, SAndPRepository>();
+            services.AddTransient<ITradeBalanceRepository, TradeBalanceRepository>();
         }
     }
 }
