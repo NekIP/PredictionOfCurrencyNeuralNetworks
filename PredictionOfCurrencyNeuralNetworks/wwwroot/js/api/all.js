584814028b6a4bb6e6b8d9dd4cfa7967e191fa39
NekIP <Shestakov.rm@list.ru>
2018-03-13 14:33:49 +0300

    Added request api

---
+++ PredictionOfCurrencyNeuralNetworks/wwwroot/js/api/all.js
@@ -0,0 +1,1 @@
+﻿import './request-api.js';
---
+++ PredictionOfCurrencyNeuralNetworks/wwwroot/js/api/request-api.js
@@ -0,0 +1,40 @@
+﻿/**
+ * Api для асинхронного взаимодействия с сервером
+ * @param {String} url Url аддрес куда будет отправляться ajax запрос
+ * @param {String} method Тип запроса ['GET', 'POST', 'JSON', 'PUT']
+ */
+export default function RequestApi(url, method) {
+    var self = this;
+
+    function execute(parameters, callback) {
+        switch (self.method.toLowerCase()) {
+            case 'get':
+                $.get(self.url, parameters, callback);
+                break;
+            case 'post':
+                $.ajax({
+                    url: self.url,
+                    type: 'POST',
+                    data: parameters,
+                    cache: true,
+                    dataType: 'json',
+                    processData: false,
+                    contentType: false,
+                    success: callback
+                });
+                break;
+            case 'json':
+                $.getJSON(self.url, parameters, callback);
+                break;
+            case 'put':
+                $.put(self.url, parameters, callback);
+                break;
+        }
+    }
+
+    self.url = url;
+    self.method = method;
+    self.execute = execute;
+
+    return self;
+}
