d3517ce30088e1214a72e11b3e88b296129bae02
NekIP <Shestakov.rm@list.ru>
2018-03-17 21:01:08 +0300

    Added prediction of currency usd to rub class

---
+++ Business/PredictionOfCurrencyUsdToRub.cs
@@ -0,0 +1,11 @@
+﻿using DataManager;
+
+namespace Business {
+    public interface IPredictionOfCurrencyUsdToRub { }
+    public class PredictionOfCurrencyUsdToRub : IPredictionOfCurrencyUsdToRub {
+        public IDataForNeuralNetworkCollector DataForNeuralNetworkCollector { get; set; }
+        public PredictionOfCurrencyUsdToRub(IDataForNeuralNetworkCollector dataForNeuralNetworkCollector) {
+            DataForNeuralNetworkCollector = dataForNeuralNetworkCollector;
+        }
+    }
+}
