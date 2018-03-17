d76762b825b35e3ccb2203651949685a37dadf9b
NekIP <Shestakov.rm@list.ru>
2018-03-17 19:52:59 +0300

    Small changes

--- PredictionOfCurrencyNeuralNetworks/Controllers/DataManagerController.cs
+++ PredictionOfCurrencyNeuralNetworks/Controllers/DataManagerController.cs
@@ -81,6 +81,7 @@ namespace PredictionOfCurrencyNeuralNetworks.Controllers {
         }
 
         public IActionResult Index() {
+            var medel = new DataForNeuralNetworkInformation();
             return View();
         }
 
---
+++ PredictionOfCurrencyNeuralNetworks/Models/DataManager/DataForNeuralNetworkInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PredictionOfCurrencyNeuralNetworks.Models.DataManager {
+    public class DataForNeuralNetworkInformation {
+        public int Count { get; set; }
+        public double[] ExpectedValue { get; set; }
+        public double[] Dispersion { get; set; }
+    }
+}
