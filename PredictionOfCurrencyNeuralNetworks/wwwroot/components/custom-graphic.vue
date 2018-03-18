07604ac9b452f5174e07fb15d9c9ae739bcc5ff8
NekIP <Shestakov.rm@list.ru>
2018-03-18 17:12:43 +0300

    'Graphic' improvment

--- PredictionOfCurrencyNeuralNetworks/wwwroot/components/custom-graphic.vue
+++ PredictionOfCurrencyNeuralNetworks/wwwroot/components/custom-graphic.vue
@@ -1,6 +1,10 @@
 ﻿<template>
     <div class="custom-graphic">
-        <div v-bind:id="code + '-graphic'" class="graphic" v-show="graphic" v-bind:style="{ width: graphicWidth() + 'px' }"></div>
+        <div v-show="graphic">
+            <input class="range" type="range" min="0" max="100" step="0.2" v-model="left"/>
+            <input class="range" type="range" min="-100" max="0" step="0.2" v-model="right" />
+        </div>
+        <div v-bind:id="code + '-graphic'" class="graphic" v-show="graphic" v-bind:style="{ width: graphicWidth() + 'px' }" v-on:wheel="wheel"></div>
     </div>
 </template>
 <script src="../js/components/custom-graphic.js"></script>
--- PredictionOfCurrencyNeuralNetworks/wwwroot/js/components/custom-graphic.js
+++ PredictionOfCurrencyNeuralNetworks/wwwroot/js/components/custom-graphic.js
@@ -16,11 +16,43 @@
                 "#2BFE72",
                 "#2BFAFE",
                 "#FE372B"
-            ]
+            ],
+            minMax: {
+                minDate: undefined,
+                maxDate: undefined,
+                minValue: undefined,
+                maxValue: undefined
+            },
+            left: 0,
+            right: 0
         }
     },
     watch: {
         graphic: function (val) {
+            this.showHideGraphic();
+        },
+        left: function (val) {
+            if (val < 0) {
+                this.left = 0;
+            }
+            if (val > 100) {
+                this.left = 100;
+            }
+            if (100 - val < Math.abs(this.right)) {
+                this.right = -(100 - val);
+            }
+            this.showHideGraphic();
+        },
+        right: function (val) {
+            if (val < -100) {
+                this.right = -100;
+            }
+            if (val > 0) {
+                this.right = 0;
+            }
+            if (100 - Math.abs(+val) < this.left) {
+                this.left = 100 - Math.abs(+val);
+            }
             this.showHideGraphic();
         }
     },
@@ -28,33 +60,33 @@
         showHideGraphic: function () {
             d3.select("#" + this.code + '-graphic').selectAll("svg").remove();
             if (this.graphic) {
-                let minMax = {
+                this.minMax = {
                     minDate: undefined,
                     maxDate: undefined,
                     minValue: undefined,
                     maxValue: undefined
-                };
+                }
                 let converted = [];
-                for (let i = 0; i < this.data.length; i++) {
+                for (let i = Math.trunc(this.data.length * (this.left / 100)); i < Math.trunc(this.data.length * (1 - Math.abs(this.right / 100))); i++) {
                     let item = {
                         id: this.data[i].id,
                         date: this.data[i].values[0],
                         values: this.data[i].values.slice(1)
                     };
-                    if (!minMax.minDate
-                        || new Date(minMax.minDate).getTime() > new Date(item.date).getTime()) {
-                        minMax.minDate = item.date;
+                    if (!this.minMax.minDate
+                        || new Date(this.minMax.minDate).getTime() > new Date(item.date).getTime()) {
+                        this.minMax.minDate = item.date;
                     }
-                    if (!minMax.maxDate
-                        || new Date(minMax.maxDate).getTime() < new Date(item.date).getTime()) {
-                        minMax.maxDate = item.date;
+                    if (!this.minMax.maxDate
+                        || new Date(this.minMax.maxDate).getTime() < new Date(item.date).getTime()) {
+                        this.minMax.maxDate = item.date;
                     }
                     for (let j = 0; j < item.values.length; j++) {
-                        if (!minMax.minValue || minMax.minValue > item.values[j]) {
-                            minMax.minValue = item.values[j];
+                        if (!this.minMax.minValue || this.minMax.minValue > item.values[j]) {
+                            this.minMax.minValue = item.values[j];
                         }
-                        if (!minMax.maxValue || minMax.maxValue < item.values[j]) {
-                            minMax.maxValue = item.values[j];
+                        if (!this.minMax.maxValue || this.minMax.maxValue < item.values[j]) {
+                            this.minMax.maxValue = item.values[j];
                         }
                     }
                     for (let j = 0; j < item.values.length; j++) {
@@ -64,10 +96,10 @@
                         converted[j].push({ x: new Date(item.date).getTime(), y: item.values[j] });
                     }
                 }
-                let y2 = minMax.minValue;
-                let y1 = minMax.maxValue;
-                let a = new Date(minMax.minDate).getTime();
-                let b = new Date(minMax.maxDate).getTime();
+                let y2 = this.minMax.minValue;
+                let y1 = this.minMax.maxValue;
+                let a = new Date(this.minMax.minDate).getTime();
+                let b = new Date(this.minMax.maxDate).getTime();
                 let i1 = 50;
                 let j1 = 25;
                 let i2 = $("#" + this.code + '-graphic').width() - 100;
@@ -79,10 +111,10 @@
                     .y(function (d) { return yScreen(d.y); });
                 let svg = d3.select("#" + this.code + '-graphic').append("svg");
                 let scaleHorizontal = d3.scaleTime()
-                    .domain([new Date(minMax.minDate), new Date(minMax.maxDate)])
+                    .domain([new Date(this.minMax.minDate), new Date(this.minMax.maxDate)])
                     .range([i1, i2]);
                 let scaleVertical = d3.scaleLinear()
-                    .domain([minMax.minValue, minMax.maxValue])
+                    .domain([this.minMax.minValue, this.minMax.maxValue])
                     .range([j2, j1]);
                 let axisHorizontal = d3.axisBottom()
                     .scale(scaleHorizontal)
@@ -90,12 +122,31 @@
                 let axisVertical = d3.axisLeft()
                     .scale(scaleVertical)
                     .ticks(24);
-                svg.append("g")
+                let xG = svg.append("g")
+                    .attr("class", "x-axis")
                     .attr("transform", "translate(" + 0 + "," + j1 + ")")
                     .call(axisHorizontal);
-                svg.append("g")
+                let yG = svg.append("g")
+                    .attr("class", "y-axis")
                     .attr("transform", "translate(" + i1 + "," + 0 + ")")
                     .call(axisVertical);
+
+                xG.selectAll("g.x-axis g.tick")
+                    .append("line")
+                    .classed("grid-line", true)
+                    .attr("x1", 0)
+                    .attr("y1", 0)
+                    .attr("x2", 0)
+                    .attr("y2", j2 - j1);
+
+                yG.selectAll("g.y-axis g.tick")
+                    .append("line")
+                    .classed("grid-line", true)
+                    .attr("x1", 0)
+                    .attr("y1", 0)
+                    .attr("x2", i2 - i1)
+                    .attr("y2", 0);
+
                 for (let i = 0; i < converted.length; i++) {
                     svg.append("path")
                         .attr("d", line(converted[i]))
@@ -113,6 +164,11 @@
                     }
                 }
             }
+        },
+        wheel: function (event) {
+            console.log(event);
+            this.left += -(event.deltaY / 100) * 0.8;
+            this.right -= -(event.deltaY / 100) * 0.8;
         }
     }
 }
